1895faf56c0f10c09799f196fc3a446f03a2d03d
ihor-kutsenko <96702664+ihor-kutsenko@users.noreply.github.com>
2024-01-04 00:05:28 +0000

    Deploying to gh-pages from @ ihor-kutsenko/PowerPulse@93b8321d3c581e4ece0481a79e742d7d6716805e 🚀

---
+++ static/js/607.58729338.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[607],{8607:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"Exercises Page"})}}}]);
+//# sourceMappingURL=607.58729338.chunk.js.map
