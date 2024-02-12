54bfddb6727b30fa6cfbc384e28297f63e6edaf4
Rammbest <igor-kutsenko@ukr.net>
2024-02-12 23:41:07 +0200

    update NotFound Page

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -4,24 +4,24 @@ import Container from 'components/Container/Container';
 import SectionUserGateway from 'components/SectionUserGateway/SectionUserGateway';
 import HomeBG from 'components/HomeBG/HomeBG';
 import style from '../../components/HomeBG/HomeBG.module.scss';
+import styles from './NotFoundPage.module.scss';
 
 import { WELCOME_PAGE_ROUTE } from 'routes/constants';
 
 const NotFoundPage = () => {
   return (
-    <Container>
+    <Container className={styles.wrapper}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>404</h1>
+        <p className={styles.text}>
+          Sorry, you have reached a page that we could not find. It seems that
+          you are lost among the numbers and letters of our virtual space.
+          Perhaps this page went on vacation or decided to disappear into
+          another dimension. We apologize for this inconvenience.
+        </p>
+        <Link to={WELCOME_PAGE_ROUTE}>Go Home</Link>
+      </div>
       <SectionUserGateway>
-        <div
-          style={{
-            paddingTop: 50,
-            gap: 50,
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-        >
-          Error 404. Sorry, page wasn`t found.{' '}
-          <Link to={WELCOME_PAGE_ROUTE}>Back to the Home page.</Link>
-        </div>
         <HomeBG className={style.home}>
           <div></div>
         </HomeBG>
