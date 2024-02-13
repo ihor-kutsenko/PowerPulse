834c96f775db15163dab2f25fe97e9860c3a8f5f
Rammbest <igor-kutsenko@ukr.net>
2024-02-13 22:11:51 +0200

    add adaptive styles - NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,6 +1,7 @@
 import { Link } from 'react-router-dom';
 
 import Container from 'components/Container/Container';
+import Svg from 'components/Svg/Svg';
 import SectionUserGateway from 'components/SectionUserGateway/SectionUserGateway';
 import HomeBG from 'components/HomeBG/HomeBG';
 import style from '../../components/HomeBG/HomeBG.module.scss';
@@ -10,8 +11,11 @@ import { WELCOME_PAGE_ROUTE } from 'routes/constants';
 
 const NotFoundPage = () => {
   return (
-    <Container className={styles.wrapper}>
-      <div className={styles.container}>
+    <Container>
+      <div className={styles.container_left}>
+        <Link to="/">
+          <Svg iconId={'icon-logo-white'} className={styles.logo} />
+        </Link>
         <h1 className={styles.title}>404</h1>
         <p className={styles.text}>
           Sorry, you have reached a page that we could not find. It seems that
@@ -19,13 +23,17 @@ const NotFoundPage = () => {
           Perhaps this page went on vacation or decided to disappear into
           another dimension. We apologize for this inconvenience.
         </p>
-        <Link to={WELCOME_PAGE_ROUTE}>Go Home</Link>
+        <Link to={WELCOME_PAGE_ROUTE} className={styles.link}>
+          Go Home
+        </Link>
       </div>
-      <SectionUserGateway>
-        <HomeBG className={style.home}>
-          <div></div>
-        </HomeBG>
-      </SectionUserGateway>
+      <div className={styles.container_right}>
+        <SectionUserGateway>
+          <HomeBG className={style.home}>
+            <div></div>
+          </HomeBG>
+        </SectionUserGateway>
+      </div>
     </Container>
   );
 };
