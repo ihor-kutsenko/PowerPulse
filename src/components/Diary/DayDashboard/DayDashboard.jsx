dbd0cc02fc5e3d6241ca14515317e7588eff4386
Rammbest <igor-kutsenko@ukr.net>
2024-03-29 17:57:38 +0200

    update DayDashboard

--- src/components/Diary/DayDashboard/DayDashboard.jsx
+++ src/components/Diary/DayDashboard/DayDashboard.jsx
@@ -7,12 +7,38 @@ const DayDashboard = () => {
   return (
     <div className={styles.dashboard_wrapper}>
       <ul className={styles.dashboard_list}>
-        <StatisticsCard />
-        <StatisticsCard />
-        <StatisticsCard />
-        <StatisticsCard />
-        <StatisticsCard />
-        <StatisticsCard />
+        <StatisticsCard
+          statisticsName={'Daily calorie intake'}
+          statisticsIcon={'icon-fluent-food'}
+          statisticsValue={'2200'}
+          statisticsPrimary={true}
+        />
+        <StatisticsCard
+          statisticsName={'Daily physical activity'}
+          statisticsIcon={'icon-dumbbell'}
+          statisticsValue={'110 min'}
+          statisticsPrimary={true}
+        />
+        <StatisticsCard
+          statisticsName={'Calories consumed'}
+          statisticsIcon={'icon-apple'}
+          statisticsValue={'707'}
+        />
+        <StatisticsCard
+          statisticsName={'Calories burned'}
+          statisticsIcon={'icon-calories'}
+          statisticsValue={'855'}
+        />
+        <StatisticsCard
+          statisticsName={'Calories remaining'}
+          statisticsIcon={'icon-bubble'}
+          statisticsValue={'1493'}
+        />
+        <StatisticsCard
+          statisticsName={'Sports remaining'}
+          statisticsIcon={'icon-runner'}
+          statisticsValue={'85 min'}
+        />
       </ul>
       <div className={styles.warning_wrapper}>
         <Svg iconId={'icon-warning'} className={styles.warning_icon} />
